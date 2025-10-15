b6b5c5803264d502ce8e9ba73941d33c88fffceb
akbarbudikp <akbar.budi1.abkp@gmail.com>
2025-10-15 20:50:28 +0700

    Delete sandbox.js

--- server/helpers/sandbox.js
+++
@@ -1,6 +0,0 @@
-// require('dotenv').config()
-process.env.GEMINI_API_KEY=''
-
-const { VirtualTryOn } = require('./gemini')
-
-VirtualTryOn('https://img.freepik.com/free-photo/white-t-shirt-men-s-basic-wear-full-body_53876-125248.jpg?semt=ais_hybrid&w=740&q=80', 'https://macanstory.com/cdn/shop/files/Photoproduit_6_f250e8cd-8a49-45a4-a12f-90118dca5c64.jpg?v=1702118522&width=2048', 180, 80, 'L')
